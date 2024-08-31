6f51201f35ae44264f056d25649e633ee43156cb
Amit <prerna.amit.jain@gmail.com>
2024-08-31 17:50:53 +0530

    Initial Version

---
+++ src/test/java/com/learn/java8/Java8LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.java8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
